f8fe70a99cc48b37a5d10ade7832c69b1eb70e7b
jamkmak <48329089+jamkmak@users.noreply.github.com>
2019-04-03 13:47:49 -0400

    Add files via upload

---
+++ solutions/dna.py
@@ -0,0 +1,24 @@
+'''
+A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+
+An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+Given: A DNA string s of length at most 1000 nt.
+
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
+'''
+
+# Counting DNA Nucleotides (DNA)
+def DNA(string):
+    s = string.upper()
+    for n in s:
+        A = s.count('A')
+        C = s.count('C')
+        G = s.count('G')
+        T = s.count('T')
+        output = str(A)+' '+str(C)+' '+str(G)+' '+str(T)
+    return output
+# Returns a string of the number of ACGT nucleotides 
+# counted, separated by spaces.
+        
